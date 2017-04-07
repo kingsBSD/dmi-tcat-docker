d264f7fabb9047fe872c242bc91f5fed3fcb4072
Giles Richard Greenway <augeas@gmail.com>
2017-04-07 18:44:27 +0100

    URL fix. 	modified:   docker-compose.yml 	modified:   run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@ export TWITTER_CONSUMER_KEY=xxxx
 export TWITTER_CONSUMER_SECRET=xxxx
 export TWITTER_USER_TOKEN=xxxx
 export TWITTER_USER_SECRET=xxxx
+export TCAT_URL=/dmi-tcat/
 export DB_PW=admin
 export ADMIN_PW=admin
 export TCAT_PW=tcat
